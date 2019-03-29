4e1fa262b8fada887e4f78b6f0b539dc79983ee8
shivam katare <aakashkatare09@gmail.com>
2019-03-29 17:05:30 +0530

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DemoMaterialModule } from './material-module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { ForceChartComponent } from './force-chart/force-chart.component';
 import { GoogleTrendComponent } from './google-trend/google-trend.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const route:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'google',component:GoogleTrendComponent},
+  {path:'force',component:ForceChartComponent},
+  {path:'bar',component:BarChartComponent},
+  {path:'login',component:LoginComponent},
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     BarChartComponent,
     ForceChartComponent,
-    GoogleTrendComponent
+    GoogleTrendComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,DemoMaterialModule,BrowserAnimationsModule
+    BrowserModule,DemoMaterialModule,BrowserAnimationsModule,RouterModule.forRoot(route),FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
